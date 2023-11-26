1aa8a0fc8cb5c178c136daeeb28b5e86e53df57b
Eshwari Mulpuri <147031461+Eshwari20@users.noreply.github.com>
2023-11-26 18:17:34 +0530

    Add files via upload

---
+++ overleague.js
@@ -0,0 +1,11 @@
+const element =(
+    <div className = "bgContainer">
+        <h1 className="overleague-heading">Super Over League</h1>
+        <div className="imgContainer">
+            <img src = "https://assets.ccbp.in/frontend/react-js/csk-img.png" className = "image"/>
+       
+        <img src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"className = "image"/>
+    </div>
+    </div>
+);
+ReactDOM.render(element ,document.getElementById("root"));
